efef0cbfcdcf28676ddd874a733ed12e3744e0f0
Niraj <nirajp.tt.19@nitj.ac.in>
2023-08-15 11:04:26 +0530

    finished with users router

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("../models/User.js");
+const Post = require("../models/Post.js");
 const router = new express.Router();
 
 //REGISTER
@@ -27,6 +28,60 @@ router.post("/users/login", async (req, res) => {
     res.status(200).send(user);
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+
+//updating our existing database with an id
+router.patch("/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["username", "email", "password", "profilePic"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates" });
+  }
+  try {
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save;
+    res.send(req.user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+//deleting user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      try {
+        await Post.deleteMany({ username: user.username });
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted...");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } catch (err) {
+      res.status(404).json("User not found!");
+    }
+  } else {
+    res.status(401).json("You can delete only your account!");
+  }
+});
+
+//reading just a single user
+router.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).send("user not found");
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
   }
 });
 
